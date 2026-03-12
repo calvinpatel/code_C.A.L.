12101299377d945f947c4782cbd853c71033113a
calvinpatel <calvin@calvinpatel.dev>
2026-03-12 16:33:40 -0400

    initial commit

---
+++ brain_project.py
@@ -0,0 +1,83 @@
+print(r'''
+                                      /|
+                                     |\|
+                                     |||
+                                     |||
+                                     |||
+                                     |||
+                                     |||
+                                     |||
+                                  ~-[{o}]-~
+                                     |/|
+              ___                    |/|
+             ///~`     |\\_          `0'         =\\\\         . .
+            ,  |='  ,))\_| ~-_                    _)  \      _/_/|
+           / ,' ,;((((((    ~ \                  `~~~\-~-_ /~ (_/\
+         /' -~/~)))))))'\_   _/'                      \_  /'  D   |
+        (       (((((( ~-/ ~-/                          ~-;  /    \--_
+         ~~--|   ))''    ')  `                            `~~\_    \   )
+             :        (_  ~\           ,                    /~~-     ./
+              \        \_   )--__  /(_/)                   |    )    )|
+    ___       |_     \__/~-__    ~~   ,'      /,_;,   __--(   _/      |
+  //~~\`\    /' ~~~----|     ~~~~~~~~'        \-  ((~~    __-~        |
+((()   `\`\_(_     _-~~-\                      ``~~ ~~~~~~   \_      /
+ )))     ~----'   /      \                                   )       )
+  (         ;`~--'        :                                _-    ,;;(
+            |    `\       |                             _-~    ,;;;;)
+            |    /'`\     ;                          _-~          _/
+           /~   /    |    )                         /;;;''  ,;;:-~
+          |    /     / | /                         |;;'   ,''
+          /   /     |  \\|                         |   ,;(    -Tua Xiong
+        _/  /'       \  \_)                   .---__\_    \,--._______
+       ( )|'         (~-_|                   (;;'  ;;;~~~/' `;;|  `;;;\
+        ) `\_         |-_;;--__               ~~~----__/'    /'_______/
+        `----'       (   `~--_ ~~~;;------------~~~~~ ;;;'_/'
+                     `~~~~~~~~'~~~-----....___;;;____---~~
+''')
+print("One moment...Teleporting user to new world")
+rebirth = input("Who are u??\n")
+print("Ahhh right exactly, salutations " + rebirth + "!")
+
+story = input("Now would u like the long story or the tldr of.... well of all this (type L for long and S for short/tldr)\n")
+if story == "L":
+    print("hmmm right i see, you want the super long extra detailed story that goes back eons and lightyears beyond our time and minds. You must've meant to click the tldr!")
+    print("don't worry at all, it happens all the time! now let's get on with this")
+    print("tldr: u got snatched up in your dreams and now your subconscious is in this 'world', which now that I too am looking around it's quite something huh.")
+elif story == "S":
+    print("yippeee you picked the right answer. not that there really was a right or wrong but there was :)")
+    print("tldr: u got snatched up in your dreams and now your subconscious is in this 'world', which now that I too am looking around it's quite something huh.")
+
+environment = input("Does any of it ring some bells, perchance??\n")
+print("tldr of the tldr: world is your brain, quite the mess we got going on huh")
+print("but fear not! for you shall find the path to freedom right over there!")
+print("(you see a suspiciously convenient lit path with a sign saying 'Freedom dis way :)'...")
+
+journey = input("Wanna go down over yonder?(type 'y' for yes, 'n' for no")
+if journey == "y":
+    print("You ponder not for a moment longer and venture off towards the void of despair. Before you venture off, you turn around out of curiosity, only to see this giant sign:")
+elif journey == "n":
+    print("You turn around only to be met with a giant sign:")
+
+print(r'''
+
+     .--._..--.
+              ___   ( _'-_  -_.'
+          _.-'   `-._|  - :- |
+      _.-'           `--...__|
+   .-'                       '--..___
+  / `._       'someone has trust       \
+   `. `._           issues'             |
+     `. `._                            /
+       '. `._    :__________....-----'
+         `..`---'    |-_  _- |___...----..._
+                     |_....--'             `.`.
+               _...--'                       `.`.
+          _..-'                             _.'.'
+       .-'            'gimme a            _.'.'
+       |              chance pls'      _.'.'
+       |                   __....------'-'
+''')
+
+trial_1 = input("Ready to get outta here??? ('y' for yes, 'n' for no)")
+if trial_1 == "y":
+    print("You set off down the very well lit path, yadayadayada small talk, yippee we made it to the first region: THE CEREBELLUM")
